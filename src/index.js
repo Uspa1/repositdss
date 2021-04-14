7b4245dceb6ce9a0d367a7cc8c0368c692dd3b0e
Alexey Vernigor <alexey.tmaa@gmail.com>
2021-04-14 15:28:40 +0300

    Adding Files

--- src/index.js
+++ src/index.js
@@ -1,1 +1,74 @@
-import './scss/styles.scss'
+import './scss/styles.scss';
+const mockIt = '/src/mock.json';
+"use strict";
+const cardsWrapper = document.querySelector('.cards-wrapper')
+const itCards = document.querySelector('.it-cards')
+
+
+class initProject {
+	constructor() {
+
+	}
+	addBtnEventListeners() {
+		var selectedBtn = new Promise((resolve, reject) => {
+			itCards.addEventListener('click', () => resolve(1))
+		});
+		return selectedBtn
+	}
+	choseCardsScope(some) {
+		var mockUrl = new Promise((resolve, reject) => {
+			switch (some) {
+				case 1:
+					resolve(mockIt)
+			}
+		});
+		return mockUrl
+	}
+	async renderMockElements(mock) {
+		try {
+			let result = await fetch(mock);
+			let data = await result.json();
+			return data.cards;
+		}
+		catch (error) {
+			console.log(error)
+		}
+	}
+	renderCardsList(cardsScope) {
+
+		cardsScope.forEach((item, index) => {
+			cardsWrapper.innerHTML += `<div><div class="uk-card uk-card-default uk-card-body">
+				<h3 class="uk-card-title"> ${item.name}</h3>
+				</div></div>`
+		})
+	}
+	addFlipListeners() {
+		let count = 0;
+		document.querySelectorAll('.uk-card').forEach(item => {
+			item.addEventListener('click', event => {
+				if (count < 1) {
+					count++;
+					event.currentTarget.classList.toggle('flipped')
+				}
+				else {
+					if (event.currentTarget.classList.contains('flipped')) {
+
+					}
+					else {
+						alert('Ты уже выбрал предсказание, на сегодня достаточно')
+					}
+
+				}
+			})
+		})
+	}
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+	const startApp = new initProject();
+	startApp.addBtnEventListeners()
+		.then((data) => startApp.choseCardsScope(data))
+		.then((data) => startApp.renderMockElements(data))
+		.then((data) => startApp.renderCardsList(data))
+		.then(() => startApp.addFlipListeners())
+});
