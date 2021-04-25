da5883a1d6a527e6aeae7a5b567c3cd2c91745f1
satikpro <saturnpro73@gmail.com>
2021-04-25 21:32:37 +0300

    sticky header

--- src/index.js
+++ src/index.js
@@ -1,9 +1,44 @@
-import './scss/styles.scss';
-const mockIt = './dist/mock.json';
-"use strict";
-
-const cardsWrapper = document.querySelector('.cards-wrapper')
-const itCards = document.querySelector('.it-cards')
 
 
+// const mockIt = './dist/mock.json';
+// "use strict";
+// const cardsWrapper = document.getElementById('cards')
+// cardsWrapper.style.padding = '50px';
+// const url = './dist/mock.json';
+// async function getPosts(url) {
+//     let response = await fetch(url);
+//     if (response.ok) {
+//         let json = await response.json();
+//         json.cards.forEach((element, index) => {
+//             if (index < 2) {
+//                 cardsWrapper.innerHTML +=
+//                     `
+//                         <div class="uk-card uk-card-body">
+//                 <h3 class="uk-card-title"> ${element.imgUrl}</h3>
+//             </div> `
+//             }
+//         });
+//     } else {
+//         alert("Ошибка HTTP: " + response.status);
+//     }
+// }
+// getPosts(url)
 
+window.addEventListener('scroll', function(){
+    const logoImage = document.querySelector
+    ('.logo img');
+    const mainNav = document.getElementById
+    ('mainNav');
+   
+    if(window.pageYOffset >  0){
+        logoImage.style.height = "68px";
+       
+        mainNav.classList.add('font-size')
+    } else {
+        logoImage.style.height = "78px";
+        
+        mainNav.classList.remove('font-size')
+    }
+    
+
+});
